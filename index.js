8a33072a3b2a2c8fef41fd6cd4d7f8dfb13cb5ea
Biemnet <Biemmet.a@gmail.com>
2023-02-18 14:09:19 -1000

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+// import your HTTP
+const { log } = require("console");
+const http = require("http");
+
+// create server with HTTP
+const server = http.createServer((req, res) => {
+  console.log("server is created");
+});
+
+// initiate a port
+const PORT = 4000;
+
+// listen to server
+server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
